c45beb5b06b2404133d1bffabffb3d39e200423a
oyvindbusk <oyvindbusk@gmail.com>
2025-09-21 11:49:57 +0200

    Flyttet config ut i egen fil

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCOvEsuUyyNwHJT3fu2BMuaLLig-9gd6NU",
+    authDomain: "buskstodo.firebaseapp.com",
+    projectId: "buskstodo",
+    storageBucket: "buskstodo.firebasestorage.app",
+    messagingSenderId: "1063381051547",
+    appId: "1:1063381051547:web:c03c2183bcfefa84f9be3e"
+};
